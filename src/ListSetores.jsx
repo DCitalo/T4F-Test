3417f6454769789735a3342b3480c0c00040bd9b
DCitalo <cd_italo@hotmail.com>
2018-01-18 20:13:12 -0200

    Layout 2.6.0

--- src/ListSetores.jsx
+++ src/ListSetores.jsx
@@ -17,43 +17,43 @@ class ListSetores extends Component {
     	    	ShowSet.map((p) =>{
     	    		return(
     	    			<li className='container-50 tb-container-90 unit-container DF FW' key={p.id}>
-                            <div className='ticket'> 
-                                <div className='top'>
-                                    <div className='ticket-head'>
-                                        <p className='bandname'><b>{BandName}</b></p>
-                                        <span className='tourname'>{NomeShow}</span>
-                                    </div>
-                                    <div>
-                                        <img src='Images/bg_intro_desktop.png' alt='' />
-                                    </div>
-                                  <div className='deetz DF FW'>
-                                    <div className='label-cont container-80'>
-                                      <div className='label-top'>{ShowLocal}</div>
-                                      <div className='label-bot'><b>{DataShow}</b></div>
-                                    </div>
-                                    <MapInfo mapInfo={p} />
-                                    <div className='label-cont container-50'>
-                                      <div className='container-100'>
-                                        <div className='label-top'>Setor</div>
-                                        <div className='label-bot'><b>{p.NomeSetor}</b></div>
-                                      </div> 
-                                      <div className='container-100'>
-                                        <div className='label-top'>Preço</div>
-                                        <div className='label-bot'><b>R${p.Valor}</b></div>
-                                      </div> 
-                                    </div>
-                                    <div className='label-cont container-50'>
-                                      <span className='label-top'>Quantidade:</span>
-                                      <NumericInput min={1} max={50} value={1}/>
-                                    </div>
-                                  </div> 
-                               </div>
-                               <div className='rip'></div>
-                               <div className='bottom DF FW'>
-                                  <div className='barcode container-50'></div>
-                                  <a className='buy container-50 text-center' href=''>Comprar</a>
-                               </div>
-                            </div>
+                  <div className='ticket'> 
+                      <div className='top'>
+                          <div className='ticket-head'>
+                              <p className='bandname'><b>{BandName}</b></p>
+                              <span className='tourname'>{NomeShow}</span>
+                          </div>
+                          <div>
+                              <img src='Images/bg_intro_desktop.png' alt='' />
+                          </div>
+                        <div className='deetz DF FW'>
+                          <div className='label-cont container-80'>
+                            <div className='label-top'>{ShowLocal}</div>
+                            <div className='label-bot'><b>{DataShow}</b></div>
+                          </div>
+                          <MapInfo mapInfo={p} />
+                          <div className='label-cont container-50'>
+                            <div className='container-100'>
+                              <div className='label-top'>Setor</div>
+                              <div className='label-bot'><b>{p.NomeSetor}</b></div>
+                            </div> 
+                            <div className='container-100'>
+                              <div className='label-top'>Preço</div>
+                              <div className='label-bot'><b>R${p.Valor}</b></div>
+                            </div> 
+                          </div>
+                          <div className='label-cont container-50'>
+                            <span className='label-top'>Quantidade:</span>
+                            <NumericInput min={1} max={6} value={1}/>
+                          </div>
+                        </div> 
+                     </div>
+                     <div className='rip'></div>
+                     <div className='bottom DF FW'>
+                        <div className='barcode container-50'></div>
+                        <a className='buy container-50 text-center' href=''>Comprar</a>
+                     </div>
+                  </div>
     	    			</li>
     	    		)
     	    	})	
